b55ca1aae75d05f3e61516f8a3339091d138809f
Justin D <justin@baal-software.com>
2019-06-03 07:33:51 -0600

    Adjusts test to check for multiple addresses

--- spec/features/admin/user_show_spec.rb
+++ spec/features/admin/user_show_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe 'Admin User Show' do
   describe 'as an admin' do
     it 'sees the same info a user sees, without option to edit' do
       user = create(:user)
+      create(:address, user: user)
       admin = create(:admin)
 
       allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
@@ -14,8 +15,10 @@ RSpec.describe 'Admin User Show' do
         expect(page).to have_content("Role: #{user.role}")
         expect(page).to have_content("Email: #{user.email}")
         within '#address-details' do
-          expect(page).to have_content("Address: #{user.address}")
-          expect(page).to have_content("#{user.city}, #{user.state} #{user.zip}")
+          user.addresses.each do |address|
+          expect(page).to have_content("Address: #{address.street_address}")
+          expect(page).to have_content("#{address.city}, #{address.state} #{address.zip}")
+          end
         end
         expect(page).to_not have_link('Edit Profile Data')
       end

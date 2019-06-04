075b53952eba89bab85ba107a0ce767718dee095
James Cape <james.cape@gmail.com>
2019-06-04 10:57:01 -0600

    Adds discount creation action in controller and adds discount display to view

--- app/controllers/dashboard/discounts_controller.rb
+++ app/controllers/dashboard/discounts_controller.rb
@@ -4,8 +4,15 @@ class Dashboard::DiscountsController < Dashboard::BaseController
   end
 
   def create
-
-
+    @discount = current_user.discounts.new(discount_params)
+    @discount.save
+    flash[:success] = "Your discount has been created!"
     redirect_to dashboard_path
+  end
+
+  private
+
+  def discount_params
+    params.require(:discount).permit(:description, :minimum_quantity, :discount_amount)
   end
 end
